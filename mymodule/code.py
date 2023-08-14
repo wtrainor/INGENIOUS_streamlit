1c6367488a16e0fe0f3602d509d38e803705de5b
wtrainor <wtrainor@gmail.com>
2023-08-14 13:05:13 -0700

    back to None

--- mymodule/code.py
+++ mymodule/code.py
@@ -127,8 +127,8 @@ def optimal_bin(X_train, y_train):
     maxValue = x_d[-1]
     bandwidths = np.linspace(0, maxValue, 20)
     grid = GridSearchCV(KDEClassifier(), {'bandwidth': bandwidths})
-    print('X_train',np.shape(X_train),np.__version__,sns.__version__)
-    grid.fit(X_train[:,None].to_numpy(), y_train) # removed 
+    st.write('X_train',np.shape(X_train),np.__version__,sns.__version__)
+    grid.fit(X_train[:,None],y_train) # removed  ,  .to_numpy() doesn't work np.reshape(,(-1,1)), y_train
     scores = grid.cv_results_['mean_test_score']
     print(grid.best_params_)
     print('accuracy =', grid.best_score_)
